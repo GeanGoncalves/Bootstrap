7df3d3b2cccd208eb932aff9f6f1c5fe061ea748
geancarlosgmartins@hotmail.com <geancarlosgmartins@hotmail.com>
2022-02-16 11:09:49 -0300

    Aula 9

--- js/scripts.js
+++ js/scripts.js
@@ -103,6 +103,7 @@ step: function(state, circle) {
   setTimeout(function(){
 
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
 
   }, 250);
 });
